c8bdff9339d41c6bf6a14e2e37e01353c5458e07
Binay Tripathy <binaytripathy123@gmail.com>
2022-11-30 16:10:30 +0530

    Done Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,6 @@
 import React
-// , { Component } 
-from 'react'
-import sapling from './../components/sapling2.jpg'
+    // , { Component } 
+    from 'react'
 import './../index.css';
 
 // export class Login extends Component {
@@ -49,31 +48,34 @@ import './../index.css';
 function Login() {
     return (
         <div>
-            <section class="vh-100" style={{ backgroundImage: `url(${sapling})`, backgroundRepeat: " no-repeat", backgroundSize: "cover" }}>
+            <section class="vh-100 logimg">
                 <div class="container py-5 h-100">
                     <div class="row d-flex justify-content-center align-items-center h-100">
                         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                            <div class="card shadow-4-strong bg-transparent text-light" style={{ borderRadius: "1rem" }}>
+                            <div class="card shadow-4-strong bg-transparent text-light" style={{ borderRadius: "1rem", border: "none" }}>
                                 <div class="card-body p-5 text-center">
 
-                                    <h3 class="mb-5">Sign in</h3>
+                                    <h2 class="mb-5">Log in</h2>
 
                                     <div class="form-outline mb-4">
-                                        <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
-                                        <label class="form-label" htmlFor="typeEmailX-2">Email</label>
+                                        <input type="email" id="typeEmailX-2" class="form-control form-control-lg" placeholder='Email' />
                                     </div>
 
                                     <div class="form-outline mb-4">
-                                        <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
-                                        <label class="form-label" htmlFor="typePasswordX-2">Password</label>
+                                        <input type="password" id="typePasswordX-2" class="form-control form-control-lg" placeholder='Password' />
+
                                     </div>
 
-                                    <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+                                    <button class="btn btn-success btn-lg btn-block" type="submit">Login</button>
 
-                                    <hr class="my-4" />
+                                    {/* <hr class="my-4" /> */}
+                                    <div class="divider d-flex align-items-center my-4">
+                                        <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
+                                    </div>
 
-                                    <button class="btn btn-lg btn-block btn-primary" style={{ backgroundColor: "#dd4b39" }}
-                                        type="submit"><i class="fab fa-google me-2"></i> Sign in with google</button>
+                                    <button class="btn btn-lg btn-block" style={{ backgroundColor: "#dd4b39" }}
+                                        type="submit"><i class="fab fa-google me-2"></i> Log In with google
+                                    </button>
 
                                 </div>
                             </div>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -11,7 +11,71 @@ import React from 'react'
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <div>
+      <section className="vh-100 gradient-custom">
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div className="card shadow-4-strong card-registration bg-transparent text-light" style={{ borderRadius: "1rem", border: "none" }}>
+                <div className="card-body p-5 text-center">
+                  <h2 className="mb-4 pb-2 pb-md-0 mb-md-5">Sign Up</h2>
+
+                  <form>
+
+                    <div className="row" style={{ justifyContent: "center" }}>
+                      <div className="col-md-6 mb-4">
+
+                        <div className="form-outline">
+                          <input type="text" id="firstName" className="form-control form-control-lg" placeholder='First Name' />
+                        </div>
+
+                      </div>
+                      <div className="col-md-6 mb-4">
+
+                        <div className="form-outline">
+                          <input type="text" id="lastName" className="form-control form-control-lg" placeholder='Last Name' />
+                        </div>
+
+                      </div>
+                    </div>
+
+                    <div className="row" style={{ justifyContent: "center" }}>
+                      <div className="col-md-6 mb-4">
+                        <div className="form">
+                          <input type="email" id="emailAddress" className="form-control form-control-lg" placeholder='Email Id' />
+                        </div>
+                      </div>
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
+
+                          <input type="password" id="typePasswordX-2" class="form-control form-control-lg" placeholder='Password' />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row" style={{ justifyContent: "center" }}>
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
+                          <input type="tel" id="phoneNumber" className="form-control form-control-lg" placeholder='Phone Number' />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="pt-2">
+                      <input className="btn btn-success btn-lg" type="submit" value="Register" />
+                    </div>
+                    <div class="divider d-flex align-items-center my-4">
+                      <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
+                    </div>
+                    <button class="btn btn-lg btn-block" style={{ backgroundColor: "#dd4b39" }}
+                      type="submit"><i class="fab fa-google me-2"></i> Sign Up with Google
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
