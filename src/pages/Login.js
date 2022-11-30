55004de4ff0d453e7a04f68d4243eda225bcddfd
Arpitam Saha <arpitam972@gmail.com>
2022-11-30 17:27:33 +0530

    backend for login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,10 @@ import React
 from 'react'
 import sapling from './../components/sapling2.jpg'
 import './../index.css';
+import { useState } from 'react';
+import { auth } from '../firebase-config';
+import {signInWithGoogle} from 'firebase'
+import {signInWithEmailAndPassword} from 'firebase/auth'
 
 // export class Login extends Component {
 //     render() {
@@ -47,6 +51,20 @@ import './../index.css';
 
 
 function Login() {
+
+    const[loginEmail , setLoginEmail] = useState("");
+    const[loginPassword , setLoginPassword] = useState("");
+
+    const login = async () => {
+        try{
+            const user = await signInWithEmailAndPassword(
+                auth , loginEmail , loginPassword
+            );
+        }catch(error){
+            console.log(error.message);
+        }
+    }
+
     return (
         <div>
             <section class="vh-100" style={{ backgroundImage: `url(${sapling})`, backgroundRepeat: " no-repeat", backgroundSize: "cover" }}>
@@ -59,21 +77,21 @@ function Login() {
                                     <h3 class="mb-5">Sign in</h3>
 
                                     <div class="form-outline mb-4">
-                                        <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
+                                        <input type="email" id="typeEmailX-2" class="form-control form-control-lg" onChange={(event)=> {setLoginEmail(event.target.value)}}/>
                                         <label class="form-label" htmlFor="typeEmailX-2">Email</label>
                                     </div>
 
                                     <div class="form-outline mb-4">
-                                        <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
+                                        <input type="password" id="typePasswordX-2" class="form-control form-control-lg" onChange={(event)=> {setLoginEmail(event.target.value)}}/>
                                         <label class="form-label" htmlFor="typePasswordX-2">Password</label>
                                     </div>
 
-                                    <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+                                    <button class="btn btn-primary btn-lg btn-block" type="submit" onClick={login}>Login</button>
 
                                     <hr class="my-4" />
 
                                     <button class="btn btn-lg btn-block btn-primary" style={{ backgroundColor: "#dd4b39" }}
-                                        type="submit"><i class="fab fa-google me-2"></i> Sign in with google</button>
+                                        type="submit" onClick={signInWithGoogle}><i class="fab fa-google me-2"></i> Login with google</button>
 
                                 </div>
                             </div>
